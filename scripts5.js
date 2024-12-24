d360da6ca4b943714b2cde225fe5c85e0b01e70b
pedrodavasco <posb1@aluno.ifal.edu.br>
2024-12-23 22:35:53 -0300

    scripts5.js

---
+++ scripts5.js
@@ -0,0 +1,13 @@
+let numeros = [4, 10, 2, 5, 7, 1];
+
+let maior = numeros[0];
+
+for (let i = 1; i < numeros.length; i++) {
+
+    if (numeros[i] > maior) {
+
+        maior = numeros[i];
+    }
+}
+
+alert("O maior número e: " + maior);
